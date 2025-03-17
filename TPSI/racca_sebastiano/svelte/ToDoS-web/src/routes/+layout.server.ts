a34b70b7902404aaf7b1b3c0650ab3bf7e72d9d1
Sebastiano <racca.sebastiano.7805@gmail.com>
2025-03-17 12:38:21 +0100

    Remove ToDos-web

--- TPSI/racca_sebastiano/svelte/ToDoS-web/src/routes/+layout.server.ts
+++
@@ -1,5 +0,0 @@
-import { dev } from "$app/environment";
-
-export const prerender = true;
-export const ssr = dev; // in dev per avere il reload automatico, altrimenti è una webapp, non è necessario
-export const csr = true;
